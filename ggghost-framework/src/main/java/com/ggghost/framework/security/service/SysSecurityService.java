4e467050763944bc960e726e1aafb1b58abaa1c1
ggghost <826842941@qq.com>
2023-10-06 19:05:35 +0800

    1

--- ggghost-framework/src/main/java/com/ggghost/framework/security/service/SysSecurityService.java
+++ ggghost-framework/src/main/java/com/ggghost/framework/security/service/SysSecurityService.java
@@ -1,9 +1,43 @@
 package com.ggghost.framework.security.service;
 
+import com.ggghost.common.constant.CacheConstants;
 import com.ggghost.common.core.redis.RedisCache;
+import com.ggghost.framework.security.AuthenticationContextHolder;
+import com.ggghost.framework.system.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SysSecurityService {
+
+    @Value(value = "5")
+    private int maxRetryCount;
+
+    @Value(value = "10")
+    private int lockTime;
+
+    @Autowired(required = false)
+    private RedisCache redisCache;
+
+    public void validate(SysUser sysUser) {
+        Authentication usernamePasswordAuthentication = AuthenticationContextHolder.getContext();
+        String name = usernamePasswordAuthentication.getName();
+        String password = usernamePasswordAuthentication.getCredentials().toString();
+
+        Integer retryCount = redisCache.getCacheObject(getCacheKey(name));
+
+        if (retryCount == null) retryCount = 0;
+        if (retryCount >= maxRetryCount) {
+
+        }
+    }
+
+    private String getCacheKey(String name) {
+        return CacheConstants.PWD_ERR_CNT_KEY + name;
+    }
+
+
+
 }
