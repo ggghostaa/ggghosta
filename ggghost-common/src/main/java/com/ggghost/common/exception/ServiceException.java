8f627ff5a864953f12db5bdf69d07263bf46040d
qzh <826842941@qq.com>
2023-10-06 19:21:49 +0800

    1

--- ggghost-common/src/main/java/com/ggghost/common/exception/ServiceException.java
+++ ggghost-common/src/main/java/com/ggghost/common/exception/ServiceException.java
@@ -4,46 +4,76 @@ package com.ggghost.common.exception;
  * @author ggghost
  * @create 2023/9/20 22:15
  */
-public class ServiceException extends RuntimeException{
-    private static final Long serialVersionUID = 1L;
+
+/**
+ * 业务异常
+ *
+ * @author ruoyi
+ */
+public final class ServiceException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
     /**
      * 错误码
      */
     private Integer code;
 
     /**
-     * 错误信息
+     * 错误提示
      */
     private String message;
 
     /**
-     * 错误信息详情
+     * 错误明细，内部调试错误
+     *
+     * 和 {@link CommonResult#getDetailMessage()} 一致的设计
      */
     private String detailMessage;
 
-    public ServiceException(Integer code, String message) {
+    /**
+     * 空构造方法，避免反序列化问题
+     */
+    public ServiceException()
+    {
+    }
+
+    public ServiceException(String message)
+    {
+        this.message = message;
+    }
+
+    public ServiceException(String message, Integer code)
+    {
+        this.message = message;
         this.code = code;
-        this.message = message;
     }
 
-    public void setDetailMessage(String detailMessage) {
-        this.detailMessage = detailMessage;
-    }
-
-    public ServiceException(String message) {
-        this.message = message;
-    }
-
-    public Integer getCode() {
-        return code;
+    public String getDetailMessage()
+    {
+        return detailMessage;
     }
 
     @Override
-    public String getMessage() {
+    public String getMessage()
+    {
         return message;
     }
 
-    public String getDetailMessage() {
-        return detailMessage;
+    public Integer getCode()
+    {
+        return code;
+    }
+
+    public ServiceException setMessage(String message)
+    {
+        this.message = message;
+        return this;
+    }
+
+    public ServiceException setDetailMessage(String detailMessage)
+    {
+        this.detailMessage = detailMessage;
+        return this;
     }
 }
